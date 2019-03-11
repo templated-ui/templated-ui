35f7dc054a3c7070dad62bad3aa88acca1323221
Majid Taheri(I love optimization) <taheri24@users.noreply.github.com>
2019-03-12 00:30:50 +0330

    add precode single-view template

--- src/templates/single-view.tsx
+++ src/templates/single-view.tsx
@@ -1,17 +1,27 @@
-import { CustomStoreManager, Action } from "../core/store";
-import { ITemplateProps } from "../core/exports";
+import { CustomStoreManager, Action, useEventManager, IStore } from "../core/store";
+import { ITemplateProps, registerTemplate, registerReducer } from "../core/templated-ui";
+const INITIALIZE = 'INITIALIZE';
 interface IState {
     type: string;
 }
 class StoreManager extends CustomStoreManager<IState>
 {
 
+    save() {
+
+    }
+
 }
 function reducer(prevState: IState, action: Action<any>): IState {
-    if (action.type == 'INITIALIZE')
+    if (action.type == INITIALIZE)
         return { type: 'single-view' };
     return prevState;
 }
 function Template(p: ITemplateProps<IState>) {
-    
+    const events = useEventManager(StoreManager);
+     
+    return <></>
 }
+
+registerTemplate('single-view', Template);
+registerReducer('single-view', reducer);
