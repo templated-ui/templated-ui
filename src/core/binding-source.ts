d30de8882878024eec9b0a03617455d4a27fbf9e
majid_taheri <majid@taheri24.ir>
2019-03-12 12:20:28 +0330

    update binding-source

--- src/core/binding-source.ts
+++ src/core/binding-source.ts
@@ -1,8 +1,19 @@
 import { useContext } from "react";
 import { storeContext } from "./store";
+export type SchemaDefination = { [K: string]: 'number' | 'string' | 'boolean' | SchemaDefination | SchemaDefination[] };
+interface IOptionsForBindingSource {
+    schema: SchemaDefination | SchemaDefination[];
+    parent: [string, IBindingSource<any>];
 
-export function openBindingSource<T>() {
-    return function (key: keyof T) {
-        const store = useContext(storeContext)
-    }
+}
+interface IBindingSource<T> {
+    (key: keyof T): any;
+    options: IOptionsForBindingSource;
+}
+export function openBindingSource<T>(options?: IOptionsForBindingSource): IBindingSource<T> {
+    options = { schema: null, ...options } as IOptionsForBindingSource;
+    return Object.assign(function (key: keyof T) {
+        const store = useContext(storeContext);
+
+    }, { options });
 }
