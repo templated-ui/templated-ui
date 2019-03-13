4f1ae71c1c1ee1e50efe2c79928edb82a9f9fbb9
majid_taheri <majid@taheri24.ir>
2019-03-13 15:00:05 +0330

    binding-source

--- src/core/binding-source.ts
+++ src/core/binding-source.ts
@@ -1,19 +1,31 @@
 import { useContext } from "react";
 import { storeContext } from "./store";
-export type SchemaDefination = { [K: string]: 'number' | 'string' | 'boolean' | SchemaDefination | SchemaDefination[] };
+import { JSONSchema6 } from 'json-schema';
+export type SchemaDefination = {
+    [K: string]:
+    'number' | 'string' | 'boolean' | JSONSchema6 | SchemaDefination[]
+};
 interface IOptionsForBindingSource {
-    schema: SchemaDefination | SchemaDefination[];
-    parent: [string, IBindingSource<any>];
+    master?: IBindingSource<any>;
 
 }
 interface IBindingSource<T> {
     (key: keyof T): any;
+    schema: SchemaDefination;
     options: IOptionsForBindingSource;
 }
-export function openBindingSource<T>(options?: IOptionsForBindingSource): IBindingSource<T> {
-    options = { schema: null, ...options } as IOptionsForBindingSource;
+export function openBindingSource
+    <T extends SchemaDefination>(schema: T, options?: IOptionsForBindingSource): IBindingSource<T> {
+    options = { ...options } as IOptionsForBindingSource;
     return Object.assign(function (key: keyof T) {
         const store = useContext(storeContext);
 
-    }, { options });
+    }, { options, schema });
 }
+const a = {
+    ss: {
+        type: 'string',
+        default: true
+    }, x: 'xsXX'
+} as SchemaDefination;
+openBindingSource(a);
