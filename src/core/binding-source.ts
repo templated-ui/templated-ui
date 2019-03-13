7fef3a84f143dac9dca5b6c447e0817d03bece44
majid_taheri <majid@taheri24.ir>
2019-03-13 15:26:42 +0330

    Update binding-source.ts

--- src/core/binding-source.ts
+++ src/core/binding-source.ts
@@ -1,31 +1,44 @@
 import { useContext } from "react";
 import { storeContext } from "./store";
 import { JSONSchema6 } from 'json-schema';
-export type SchemaDefination = {
-    [K: string]:
-    'number' | 'string' | 'boolean' | JSONSchema6 | SchemaDefination[]
+
+export type SchemaDefination<T, KT extends keyof T> = {
+    [key in KT]:
+    string | JSONSchema6 | SchemaDefination<T[key], keyof T[key]>[]
 };
+function Schema<T>(schema: T): SchemaDefination<T, keyof T> {
+    const _schema = schema as any;
+    if (_schema['__schema'])
+        return _schema;
+    const entries = Object.entries(_schema);
+    return Object.assign({ __schema: true },
+        ...entries.map(([key, value]) => ([key, typeof value])).map(([key, value]) => ({ [key]: value })))
+
+}
+
 interface IOptionsForBindingSource {
     master?: IBindingSource<any>;
 
 }
 interface IBindingSource<T> {
     (key: keyof T): any;
-    schema: SchemaDefination;
+    schema: SchemaDefination<T, keyof T>;
     options: IOptionsForBindingSource;
 }
-export function openBindingSource
-    <T extends SchemaDefination>(schema: T, options?: IOptionsForBindingSource): IBindingSource<T> {
+ 
+export function openBindingSource<T>
+    (schema: T, options?: IOptionsForBindingSource): IBindingSource<T> {
     options = { ...options } as IOptionsForBindingSource;
     return Object.assign(function (key: keyof T) {
         const store = useContext(storeContext);
 
     }, { options, schema });
 }
-const a = {
-    ss: {
-        type: 'string',
-        default: true
-    }, x: 'xsXX'
-} as SchemaDefination;
-openBindingSource(a);
+// const a = Schema({
+//     ss: {
+//         type: 'string',
+//         default: true
+//     }, x: 'string'
+// });
+
+ 
